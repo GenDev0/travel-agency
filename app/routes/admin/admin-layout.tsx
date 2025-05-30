793510e26455d4e4756db7d846e19c17bdcb9742
Personal Dev <dev.gen.info.0@gmail.com>
2025-05-30 17:45:24 +0100

    feat(sidebar): add desktop sidebar

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { NavItems } from 'components';
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='admin-layout'>
         Mobile Sidebar
-        <aside className='w-full max-w-[270px] hidden lg:block'>Sidebar Content</aside>
+        <aside className='w-full max-w-[270px] hidden lg:block'>
+          <SidebarComponent width={"270px"} enableGestures={false}>
+            <NavItems />
+          </SidebarComponent>
+        </aside>
         <aside className='children'>
             <Outlet />
         </aside>
---
+++ components/navItems.tsx
@@ -0,0 +1,49 @@
+import { cn } from "lib/utils"
+import { Link, NavLink } from "react-router"
+import { sidebarItems } from "~/constants"
+
+const NavItems = () => {
+
+    const user = {
+        name: "Ahmed Chebbi",
+        email: "ahmed@chebbi.com",
+        imageUrl: "/assets/images/david.webp"
+
+    }
+
+  return (
+    <section className="nav-items">
+        <Link to="/" className="link-logo">
+          <img className="size-[30px]" src="/assets/icons/logo.svg" alt="logo" />
+          <h1>Tourismo</h1>
+        </Link>
+        <div className="container">
+            <nav>
+                {sidebarItems.map(({id,href,icon,label}) => (
+                   <NavLink key={id} to={href}>
+                       {({isActive}) => (
+                           <div className={cn(
+                               "group nav-item", {"bg-primary-100 !text-white": isActive})}>
+                               <img src={icon} alt={label} className={`group-hover:brightness-0 size-0 group-hover:invert ${isActive ? 'brightness-0 invert' : 'text-dark-200'}`}/>
+                               <span>{label}</span>
+                           </div>
+                       )}
+                   </NavLink>
+                ))}
+            </nav>
+            <footer className="nav-footer">
+                <img src={user?.imageUrl || "/assets/images/david.webp"} alt={user?.name || "User"} />
+                <article>
+                    <h2>{user?.name || "User"}</h2>
+                    <p>{user?.email || "user@example.com"}</p>
+                </article>
+                <button onClick={() => console.log("Logout")} className="cursor-pointer">
+                    <img src="/assets/icons/logout.svg" alt="Logout" className="size-6"/>
+                </button>
+            </footer>
+        </div>
+    </section>
+  )
+}
+
+export default NavItems
