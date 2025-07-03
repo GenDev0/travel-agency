6ed3bedc5666e4a64bd5aaba4cff303c5c8e59b0
GenDev0 <dev.Gen.info0@gmail.com>
2025-07-03 12:07:20 +0100

    Rename navitems.tsx to nav-items.tsx

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
-export { default as NavItems } from "./NavItems";
+export { default as NavItems } from "./nav-items";
 export { default as MobileSidebar } from "./MobileSidebar";
 export { default as Header } from "./Header";
 export { default as TripCard } from "./TripCard";
